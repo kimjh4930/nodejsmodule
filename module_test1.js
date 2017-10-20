b209bc114f9e316c18d9daccaf8ec6a449193118
Kim, Junha <kimjh4930@gmail.com>
2017-10-20 14:31:43 +0900

    allocate only function

--- module_test1.js
+++ module_test1.js
@@ -8,7 +8,7 @@ console.dir(user);
 
 
 function showUser(){
-	return user.getUser().name + ', ' + user.group.name;
+	return user.getUser().name + ', No Group';
 };
 
 console.log('사용자정보 : %s', showUser());
