1ffb71a0e1a6f5cf543df05da96631cf115a313d
Kim, Junha <kimjh4930@gmail.com>
2017-10-20 15:34:53 +0900

    객체인스턴스를 만들어 모듈로 분리.

--- module_test1.js
+++ module_test1.js
@@ -4,8 +4,4 @@
 
 var user = require('./user1');
 
-function showUser(){
-	return user.getUser().name + ',' + user.group.name;
-}
-
-console.log('사용자정보 : %s', showUser());
+user.printUser();
--- user1.js
+++ user1.js
@@ -1,7 +1,16 @@
-module.exports = {
-		getUser : function(){
-			return {id : 'test01', name : '소녀시대.'};
-		},
-		group : {id : 'group01', name : '친구'}
+function User(id, name){
+	this.id = id;
+	this.name = name;
 }
- exports.group = {id : 'group02', name : '가족'};
+
+User.prototype.getUser = function(){
+	return { id:this.id, name : this.name };
+}
+
+User.prototype.group = {id : 'group1', name : '친구'};
+
+User.prototype.printUser = function(){
+	console.log('user 이름 : %s, group 이름 : %s', this.name, this.group.name);
+}
+
+module.exports = new User('test01', '소녀시대');
