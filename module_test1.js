a9272d924c53eb270977ca718b65cd78c940e15c
Kim, Junha <kimjh4930@gmail.com>
2017-10-20 15:18:03 +0900

    fix miss type

--- module_test1.js
+++ module_test1.js
@@ -5,7 +5,7 @@
 var user = require('./user1');
 
 function showUser(){
-	return user.gerUser().name + ',' + user.group.name;
+	return user.getUser().name + ',' + user.group.name;
 }
 
 console.log('사용자정보 : %s', showUser());
